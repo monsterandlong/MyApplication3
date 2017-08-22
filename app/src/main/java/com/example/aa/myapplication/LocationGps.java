fc38abcb10af3e7e82634d05a5945ef6375b4bf7
monsterandlong <zhangzhilong120>
2017-08-22 14:12:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aa/myapplication/LocationGps.java
@@ -0,0 +1,14 @@
+package com.example.aa.myapplication;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+
+
+/**
+ * Created by 众云图-1 on 2017/8/21.
+ */
+
+public class LocationGps {
+
+}
---
+++ app/src/main/java/com/example/aa/myapplication/Main2Activity.java
@@ -0,0 +1,18 @@
+package com.example.aa.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.baidu.location.BDLocation;
+
+/**
+ * Created by 众云图-1 on 2017/8/21.
+ */
+
+public class Main2Activity{
+
+}
